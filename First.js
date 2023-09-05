cbbf564a040a106b8bb3c71c6748a1d208ce9005
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-09-05 23:46:03 +0530

    Adding few more line of the bacis code

--- First.js
+++ First.js
@@ -1,1 +1,14 @@
 const name = 'kiran'
+var email = 'Kiran@123.com'
+let password = 'Kiran@123'
+
+email = 'kishore@123.com'
+password = 'kishore#234'
+
+console.log(`Hello ${name} This is ur First JavaScript code....!🥳`);
+console.log(email);
+// name = 'kishore' We can't assign as it is a constant.
+// console.log(password);
+console.log(`${password} is a secure password`);
+
+console.table([name, password, `This is ur email ${email}`])
